6e218587bd2668ea213d61f3ac6a4a74f7d5eb42
deb4444 <4444debasish@gmail.com>
2022-06-19 12:00:26 +0530

    newform

---
+++ reactform/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactContact from './component/ReactContact'
+import "./style.css"
+
+const App = () => {
+  return (
+    <>
+      <ReactContact/>
+    </>
+  )
+}
+
+export default App
+
